9f9f65ba9eb9746717da78ffd2ced8b073179296
Maier Ludentrop <agamemnonagross@gmail.com>
2024-08-07 01:35:27 +0300

    updated cfg.py file

--- cfg.py
+++ cfg.py
@@ -1,16 +1,13 @@
 """
 The configuration file.
 """
-# TOKEN = 'Telegram Token'
+TOKEN = 'Telegram Token'
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive",
 ]
 
 CREDENTIALS_FILE = "creds.json"  # Path to the Google credentials file
-# SPREADSHEET_ID = "ID of the Google spreadsheet"
-# TABLE_URL = "URL of the table"
+SPREADSHEET_ID = "ID of the Google spreadsheet"
+TABLE_URL = "URL of the table"
 
-TOKEN = '7247769396:AAEuGg7pZjYuHIddcKy2E8JVWvb2nthg-Q0'
-SPREADSHEET_ID = "1hC5iWOeeHKIFbAWV375CyK7J8bZKY7nATVnJQRiHEhQ"
-TABLE_URL = "https://docs.google.com/spreadsheets/d/1hC5iWOeeHKIFbAWV375CyK7J8bZKY7nATVnJQRiHEhQ/edit?gid=0#gid=0"
